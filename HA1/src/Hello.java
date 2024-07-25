88f1690d8571b62432dc0cb321deac6a2950bb6a
SaiAshishM9 <mrichigo321@gmail.com>
2024-07-25 21:39:28 +0530

    JAVA

---
+++ HA1/src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Cyrus!!");
+        System.out.print("AI-DS is best");
+        System.out.println("isn't it?");
+    }
+}
